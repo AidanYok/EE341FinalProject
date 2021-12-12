810e9dc6e6c8f3f153705a0fe2e579f1f5004ddd
Aidan Yokuda <74520882+AidanYok@users.noreply.github.com>
2021-12-12 13:16:39 -0800

    Update training.py

--- training.py
+++ training.py
@@ -9,11 +9,11 @@
 # import default python-library
 ########################################################################
 
-#import os
+import os
 import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU')
-# for gpu in gpus:
-#   tf.config.experimental.set_memory_growth(gpu, True)
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
 import glob
 import sys
 import time
