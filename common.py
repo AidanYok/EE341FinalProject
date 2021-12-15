05418a2f4e118b6d947f70eed322d1cf3aad4104
AidanYok <ayokuda@aol.com>
2021-12-14 23:23:30 -0800

    inital commit for dcase

--- common.py
+++ common.py
@@ -9,11 +9,8 @@
 # import python-library
 ########################################################################
 # default
-import csv
 import glob
 import argparse
-import itertools
-import re
 import sys
 import os
 
@@ -23,7 +20,6 @@ import librosa
 import librosa.core
 import librosa.feature
 import yaml
-from tqdm import tqdm
 
 ########################################################################
 
@@ -59,17 +55,32 @@ __versions__ = "1.0.0"
 ########################################################################
 def command_line_chk():
     parser = argparse.ArgumentParser(description='Without option argument, it will not run properly.')
-    parser.add_argument('-c', '--config', type=str, help="specify yml config")
+    parser.add_argument('-v', '--version', action='store_true', help="show application version")
+    parser.add_argument('-e', '--eval', action='store_true', help="run mode Evaluation")
+    parser.add_argument('-d', '--dev', action='store_true', help="run mode Development")
     args = parser.parse_args()
-    return args
+    if args.version:
+        print("===============================")
+        print("DCASE 2020 task 2 baseline\nversion {}".format(__versions__))
+        print("===============================\n")
+    if args.eval ^ args.dev:
+        if args.dev:
+            flag = True
+        else:
+            flag = False
+    else:
+        flag = None
+        print("incorrect argument")
+        print("please set option argument '--dev' or '--eval'")
+    return flag
 ########################################################################
 
 
 ########################################################################
 # load parameter.yaml
 ########################################################################
-def yaml_load(config):
-    with open(config) as stream:
+def yaml_load():
+    with open("baseline.yaml") as stream:
         param = yaml.safe_load(stream)
     return param
 
@@ -99,15 +110,6 @@ def file_load(wav_name, mono=False):
         logger.error("file_broken or not exists!! : {}".format(wav_name))
 
 
-def save_csv(save_file_path,
-             save_data):
-    with open(save_file_path, "w", newline="") as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows(save_data)
-
-def save_dat(data, filename):
-    numpy.savetxt(filename, data, delimiter=' ', newline='\n', fmt='%g')
-
 ########################################################################
 
 
@@ -133,7 +135,6 @@ def file_to_vector_array(file_name,
     # 01 calculate the number of dimensions
     dims = n_mels * frames
 
-
     # 02 generate melspectrogram using librosa
     y, sr = file_load(file_name)
     mel_spectrogram = librosa.feature.melspectrogram(y=y,
@@ -142,41 +143,27 @@ def file_to_vector_array(file_name,
                                                      hop_length=hop_length,
                                                      n_mels=n_mels,
                                                      power=power)
-    numpy.shape(mel_spectrogram)
-    mel_spectrogram = mel_spectrogram[:,50:250]
-    numpy.shape(mel_spectrogram)
-    
-
 
     # 03 convert melspectrogram to log mel energy
     log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
-    
-    #03.5 trim to 50 to 250
-    
 
     # 04 calculate total vector size
     vector_array_size = len(log_mel_spectrogram[0, :]) - frames + 1
-
 
     # 05 skip too short clips
     if vector_array_size < 1:
         return numpy.empty((0, dims))
 
     # 06 generate feature vectors by concatenating multiframes
-    #vector_array = numpy.zeros((vector_array_size, dims))
-    vector_array = numpy.zeros((vector_array_size, 32*4))
-    new_mels = 32
-    new_frames = 4
+    vector_array = numpy.zeros((vector_array_size, dims))
+    for t in range(frames):
+        vector_array[:, n_mels * t: n_mels * (t + 1)] = log_mel_spectrogram[:, t: t + vector_array_size].T
 
-    #skip method
-    for t in range(new_frames):
-        new_vec = log_mel_spectrogram[:, t: t + vector_array_size].T
-        vector_array[:, new_mels * t: new_mels * (t + 1)] = new_vec[:,::4]
     return vector_array
 
 
 # load dataset
-def select_dirs(param):
+def select_dirs(param, mode):
     """
     param : dict
         baseline.yaml data
@@ -189,158 +176,15 @@ def select_dirs(param):
             dirs : list [ str ]
                 load base directory list of eval_data
     """
-
-    logger.info("load_directory <- development")
-    dir_path = os.path.abspath("{base}/*".format(base=param["dev_directory"]))
-    dirs = sorted(glob.glob(dir_path))
-
+    if mode:
+        logger.info("load_directory <- development")
+        dir_path = os.path.abspath("{base}/*".format(base=param["dev_directory"]))
+        dirs = sorted(glob.glob(dir_path))
+    else:
+        logger.info("load_directory <- evaluation")
+        dir_path = os.path.abspath("{base}/*".format(base=param["eval_directory"]))
+        dirs = sorted(glob.glob(dir_path))
     return dirs
 
 ########################################################################
 
-
-def get_machine_id_list_for_test(target_dir,
-                                 dir_name="test",
-                                 ext="wav"):
-    """
-    target_dir : str
-        base directory path of "dev_data" or "eval_data"
-    test_dir_name : str (default="test")
-        directory containing test data
-    ext : str (default="wav)
-        file extension of audio files
-
-    return :
-        machine_id_list : list [ str ]
-            list of machine IDs extracted from the names of test files
-    """
-    # create test files
-    dir_path = os.path.abspath("{dir}/{dir_name}/*.{ext}".format(dir=target_dir, dir_name=dir_name, ext=ext))
-    file_paths = sorted(glob.glob(dir_path))
-    # extract id
-    machine_id_list = sorted(list(set(itertools.chain.from_iterable(
-        [re.findall('id_[0-9][0-9]', ext_id) for ext_id in file_paths]))))
-    return machine_id_list
-
-
-def test_file_list_generator(target_dir,
-                             id_name,
-                             dir_name="test",
-                             prefix_normal="normal",
-                             prefix_anomaly="anomaly",
-                             ext="wav"):
-    """
-    target_dir : str
-        base directory path of the dev_data or eval_data
-    id_name : str
-        id of wav file in <<test_dir_name>> directory
-    dir_name : str (default="test")
-        directory containing test data
-    prefix_normal : str (default="normal")
-        normal directory name
-    prefix_anomaly : str (default="anomaly")
-        anomaly directory name
-    ext : str (default="wav")
-        file extension of audio files
-
-    return :
-        if the mode is "development":
-            test_files : list [ str ]
-                file list for test
-            test_labels : list [ boolean ]
-                label info. list for test
-                * normal/anomaly = 0/1
- 
-    """
-    logger.info("target_dir : {}".format(target_dir+"_"+id_name))
-
-    # development
-    normal_files = sorted(
-        glob.glob("{dir}/{dir_name}/{prefix_normal}_{id_name}*.{ext}".format(dir=target_dir,
-                                                                                dir_name=dir_name,
-                                                                                prefix_normal=prefix_normal,
-                                                                                id_name=id_name,
-                                                                                ext=ext)))
-    normal_labels = numpy.zeros(len(normal_files))
-    anomaly_files = sorted(
-        glob.glob("{dir}/{dir_name}/{prefix_anomaly}_{id_name}*.{ext}".format(dir=target_dir,
-                                                                                dir_name=dir_name,
-                                                                                prefix_anomaly=prefix_anomaly,
-                                                                                id_name=id_name,
-                                                                                ext=ext)))
-    anomaly_labels = numpy.ones(len(anomaly_files))
-    files = numpy.concatenate((normal_files, anomaly_files), axis=0)
-    labels = numpy.concatenate((normal_labels, anomaly_labels), axis=0)
-    logger.info("test_file  num : {num}".format(num=len(files)))
-    if len(files) == 0:
-        logger.exception("no_wav_file!!")
-    print("\n========================================")
-
-    return files, labels
-########################################################################
-
-def list_to_vector_array(file_list,
-                         msg="calc...",
-                         n_mels=64,
-                         frames=5,
-                         n_fft=1024,
-                         hop_length=512,
-                         power=2.0):
-    """
-    convert the file_list to a vector array.
-    file_to_vector_array() is iterated, and the output vector array is concatenated.
-
-    file_list : list [ str ]
-        .wav filename list of dataset
-    msg : str ( default = "calc..." )
-        description for tqdm.
-        this parameter will be input into "desc" param at tqdm.
-
-    return : numpy.array( numpy.array( float ) )
-        vector array for training (this function is not used for test.)
-        * dataset.shape = (number of feature vectors, dimensions of feature vectors)
-    """
-    # calculate the number of dimensions
-    dims = n_mels * frames
-
-    # iterate file_to_vector_array()
-    for idx in tqdm(range(len(file_list)), desc=msg):
-        vector_array = file_to_vector_array(file_list[idx],
-                                                n_mels=n_mels,
-                                                frames=frames,
-                                                n_fft=n_fft,
-                                                hop_length=hop_length,
-                                                power=power)
-        if idx == 0:
-            dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
-        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
-
-    return dataset
-
-
-def file_list_generator(target_dir,
-                        dir_name="train",
-                        ext="wav"):
-    """
-    target_dir : str
-        base directory path of the dev_data or eval_data
-    dir_name : str (default="train")
-        directory name containing training data
-    ext : str (default="wav")
-        file extension of audio files
-
-    return :
-        train_files : list [ str ]
-            file list for training
-    """
-    logger.info("target_dir : {}".format(target_dir))
-
-    # generate training list
-    training_list_path = os.path.abspath("{dir}/{dir_name}/*.{ext}".format(dir=target_dir, dir_name=dir_name, ext=ext))
-    files = sorted(glob.glob(training_list_path))
-    if len(files) == 0:
-        logger.exception("no_wav_file!!")
-
-    logger.info("train_file num : {num}".format(num=len(files)))
-    return files
-########################################################################
